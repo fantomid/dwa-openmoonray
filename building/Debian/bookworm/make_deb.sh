5fb3fcbabd27c12f9622c9f7c44feb7db81eb03e
Guillaume GASNIER <2644355+fantomid@users.noreply.github.com>
2025-06-17 17:08:18 +0200

    Update make_deb.sh Add dependencies for CPU package Remove folder package-xpu

--- building/Debian/bookworm/make_deb.sh
+++ building/Debian/bookworm/make_deb.sh
@@ -20,11 +20,15 @@ if [ ! -d "$2/openmoonray" ]; then
     exit
 fi
 
+PACKAGE_DIR=$1/building/Debian/bookworm/package
+PACKAGE_CONTROL_DIR=$1/building/Debian/bookworm/package/moonray/DEBIAN
 if [ $3 = "cpu"]; then
-    PACKAGE_DIR=$1/building/Debian/bookworm/package
+    PACKAGE_CONTROL_FIELD_PACKAGE="Package: MoonRay full package (CPU only)"
+    PACKAGE_CONTROL_FIELD_DEPENDS="Depends: libglvnd0, libcgroup2, libgif7, libmng1, libjpeg62-turbo, libatomic1, libuuid1, openssl, libcurl4, libhwloc15, libfreetype6, libssl3, libjemalloc2, libblosc1, liblog4cplus-2.0.5, libcppunit-1.15-0, libmicrohttpd12, python-is-python3, lua5.4, liblua5.4-0, libqt5opengl5"
     PACKAGE_NAME=moonray
 elif [ $3 = "xpu"]; then
-    PACKAGE_DIR=$1/building/Debian/bookworm/package-xpu
+    PACKAGE_CONTROL_FIELD_PACKAGE="Package: MoonRay full package (CPU / GPU)"
+    PACKAGE_CONTROL_FIELD_DEPENDS="Depends: libglvnd0, libcgroup2, libgif7, libmng1, libjpeg62-turbo, libatomic1, libuuid1, openssl, libcurl4, libhwloc15, libfreetype6, libssl3, libjemalloc2, libblosc1, liblog4cplus-2.0.5, libcppunit-1.15-0, libmicrohttpd12, python-is-python3, lua5.4, liblua5.4-0, libqt5opengl5"
     PACKAGE_NAME=moonray-xpu
 else
     echo "$3 is invalid, package's type could be cpu or xpu, can't create the Debian's package."
@@ -32,9 +36,11 @@ else
 fi
 
 cd $PACKAGE_DIR
+awk -v valuePackage="$PACKAGE_CONTROL_FIELD_PACKAGE" -v valueDepends="$PACKAGE_CONTROL_FIELD_DEPENDS" -f $1/building/Debian/make_deb.awk < $PACKAGE_CONTROL/control.tpl > $PACKAGE_CONTROL_DIR/control
 
 mkdir -p moonray/opt/MoonRay/installs
 cp -r $2/* moonray/opt/MoonRay/installs
+cp -r $1/testdata moonray/opt/MoonRay/installs/
 
 dpkg-deb --build $PACKAGE_NAME
 VERSION=`dpkg -I $PACKAGE_NAME.deb | grep Version | cut -d ":" -f 2 | sed 's/ //g'`
